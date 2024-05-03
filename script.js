bee9b3f0d1d4f21330584a0af9b7377805beaf0d
algolkk <loplokeel+gh@gmail.com>
2024-05-03 12:29:42 +0900

    wip

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $("#value").on("change", function () {
+        var text = $(this).val();
+        var caseType = detectCaseType(text);
+
+        $("#camel").text(convert2CamelCase(text, caseType));
+        $("#snake").text(convert2SnakeCase(text, caseType));
+        $("#pascal").text(convert2PascalCase(text, caseType));
+    });
+
+    function convert2CamelCase(text, caseType) {
+        if (caseType === "camel") return text;
+        return text
+            .replace(/[-_\s]([a-zA-Z0-9])/g, function (match, char) {
+                return char.toUpperCase();
+            })
+            .replace(/^([A-Z])/, function (match, char) {
+                return char.toLowerCase();
+            });
+    }
+
+    function convert2SnakeCase(text, caseType) {
+        if (caseType === "snake") return text;
+        return text
+            .replace(/[A-Z]/g, function (match) {
+                return "_" + match.toLowerCase();
+            })
+            .replace(/^_/, "");
+    }
+
+    function convert2PascalCase(text, caseType) {
+        return text
+            .replace(/(^\w|-\w|_\w|\s\w)/g, function (match) {
+                return match.charAt(match.length - 1).toUpperCase();
+            })
+            .replace(/[-_\s]/g, "");
+    }
+
+    function detectCaseType(text) {
+        if (/^[a-z][a-zA-Z0-9]*$/.test(text)) {
+            return "camel";
+        } else if (/^[A-Z][a-zA-Z0-9]*$/.test(text)) {
+            return "pascal";
+        } else if (/^[a-z]+(_[a-z]+)*$/.test(text)) {
+            return "snake";
+        } else {
+            return "unknown";
+        }
+    }
+});
