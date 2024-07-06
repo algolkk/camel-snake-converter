df5ab420d1c626ad01d3ca796052cc2d0e08b003
algolkk <loplokeel+gh@gmail.com>
2024-07-06 22:51:05 +0900

    chore: Update container size classes and layout styling

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
 $(document).ready(function () {
-    /**
-     * Convert
-     */
-    $("#convert").on("click", function () {
-        var text = $("#value").val();
-        var caseType = detectCaseType(text);
+  /**
+   * Convert
+   */
+  $("#convert").on("click", function () {
+    var text = $("#value").val();
+    var caseType = detectCaseType(text);
 
-        $("#camel").text(convert2CamelCase(text, caseType));
-        $("#snake").text(convert2SnakeCase(text, caseType));
-        $("#pascal").text(convert2PascalCase(text, caseType));
-    });
+    $("#camel").text(convert2CamelCase(text, caseType));
+    $("#snake").text(convert2SnakeCase(text, caseType));
+    $("#pascal").text(convert2PascalCase(text, caseType));
+  });
 
-    function convert2CamelCase(text, caseType) {
-        if (caseType === "camel") return text;
-        return text
-            .replace(/[-_\s]([a-zA-Z0-9])/g, function (match, char) {
-                return char.toUpperCase();
-            })
-            .replace(/^([A-Z])/, function (match, char) {
-                return char.toLowerCase();
-            });
+  function convert2CamelCase(text, caseType) {
+    if (caseType === "camel") return text;
+    return text
+      .replace(/[-_\s]([a-zA-Z0-9])/g, function (match, char) {
+        return char.toUpperCase();
+      })
+      .replace(/^([A-Z])/, function (match, char) {
+        return char.toLowerCase();
+      });
+  }
+
+  function convert2SnakeCase(text, caseType) {
+    if (caseType === "snake") return text;
+    return text
+      .replace(/[A-Z]/g, function (match) {
+        return "_" + match.toLowerCase();
+      })
+      .replace(/^_/, "");
+  }
+
+  function convert2PascalCase(text, caseType) {
+    return text
+      .replace(/(^\w|-\w|_\w|\s\w)/g, function (match) {
+        return match.charAt(match.length - 1).toUpperCase();
+      })
+      .replace(/[-_\s]/g, "");
+  }
+
+  function detectCaseType(text) {
+    if (/^[a-z][a-zA-Z0-9]*$/.test(text)) {
+      return "camel";
+    } else if (/^[A-Z][a-zA-Z0-9]*$/.test(text)) {
+      return "pascal";
+    } else if (/^[a-z]+(_[a-z]+)*$/.test(text)) {
+      return "snake";
+    } else {
+      return "unknown";
     }
+  }
 
-    function convert2SnakeCase(text, caseType) {
-        if (caseType === "snake") return text;
-        return text
-            .replace(/[A-Z]/g, function (match) {
-                return "_" + match.toLowerCase();
-            })
-            .replace(/^_/, "");
-    }
+  /**
+   * Copy to clipboard
+   */
+  $("#copyCamel").on("click", function () {
+    copyToClipboard("#camel");
+  });
 
-    function convert2PascalCase(text, caseType) {
-        return text
-            .replace(/(^\w|-\w|_\w|\s\w)/g, function (match) {
-                return match.charAt(match.length - 1).toUpperCase();
-            })
-            .replace(/[-_\s]/g, "");
-    }
+  $("#copySnake").on("click", function () {
+    copyToClipboard("#snake");
+  });
 
-    function detectCaseType(text) {
-        if (/^[a-z][a-zA-Z0-9]*$/.test(text)) {
-            return "camel";
-        } else if (/^[A-Z][a-zA-Z0-9]*$/.test(text)) {
-            return "pascal";
-        } else if (/^[a-z]+(_[a-z]+)*$/.test(text)) {
-            return "snake";
-        } else {
-            return "unknown";
-        }
-    }
+  $("#copyPascal").on("click", function () {
+    copyToClipboard("#pascal");
+  });
 
-    /**
-     * Copy to clipboard
-     */
-    $("#copyCamel").on("click", function () {
-        copyToClipboard("#camel");
-    });
+  /**
+   * Toastify
+   */
+  function copyToClipboard(sourceId) {
+    var textToCopy = $(sourceId).text();
+    navigator.clipboard.writeText(textToCopy).then(
+      function () {
+        showToast("Copied!", "success");
+      },
+      function (err) {
+        showToast("Error!", "error");
+      }
+    );
+  }
 
-    $("#copySnake").on("click", function () {
-        copyToClipboard("#snake");
-    });
-
-    $("#copyPascal").on("click", function () {
-        copyToClipboard("#pascal");
-    });
-
-    /**
-     * Toastify
-     */
-    function copyToClipboard(sourceId) {
-        var textToCopy = $(sourceId).text();
-        navigator.clipboard.writeText(textToCopy).then(
-            function () {
-                showToast("Copied!", "success");
-            },
-            function (err) {
-                showToast("Error!", "error");
-            }
-        );
-    }
-
-    function showToast(message, type) {
-        Toastify({
-            text: message,
-            duration: 3000,
-            close: true,
-            gravity: "top",
-            position: "right",
-            backgroundColor: type === "success" ? "#20c997" : "#dc3545",
-        }).showToast();
-    }
+  function showToast(message, type) {
+    Toastify({
+      text: message,
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      backgroundColor: type === "success" ? "#20c997" : "#dc3545",
+    }).showToast();
+  }
 });
